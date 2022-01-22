b7b8b4b36757be9889fbc39edde32568944c2430
Alejandro Vilchis <alexrv4299@gmail.com>
2022-01-22 02:58:22 -0600

    Add basic tests

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,22 @@ use envful::checker;
 use envful::runner;
 use std::path::PathBuf;
 
-/// Check if environment variables are present before running a process.
+/// A tool to verify the existence of environment variables before running a process.
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
+#[clap(version, about, long_about = None)]
 struct Args {
     /// Directory to look for envful.json
     #[clap(short, long, parse(from_os_str), global = true)]
     dir: Option<PathBuf>,
+
+    /// Command to execute
     #[clap(subcommand)]
     command: Commands,
 }
 
 #[derive(Subcommand, Debug)]
 enum Commands {
+    /// Check if the .env has all required variables and warns if missing
     Check,
     #[clap(external_subcommand)]
     Other(Vec<String>),
@@ -23,10 +26,7 @@ enum Commands {
 
 fn main() {
     let args = Args::parse();
-
-    let dir = args.dir;
-    let dir = dir.unwrap_or_else(|| PathBuf::from("."));
-
+    let dir = args.dir.unwrap_or_else(|| PathBuf::from("."));
     match args.command {
         Commands::Check => checker::check(&dir, true, false),
         Commands::Other(args) => runner::run(&dir, &args),
--- tests/integration.rs
+++ tests/integration.rs
@@ -1,4 +1,26 @@
 mod tests {
+    use std::process::Command;
+
     #[test]
-    fn succeeds_with_all() {}
+    fn succeeds_with_required() {
+        let status = Command::new("cargo")
+            .args(["run", "--", "check", "-d", "tests/fixtures/success"])
+            .status();
+        assert_eq!(status.unwrap().code(), Some(0));
+    }
+
+    #[test]
+    fn fails_with_missing() {
+        let status = Command::new("cargo")
+            .args(["run", "--", "check", "-d", "tests/fixtures/missing"])
+            .status();
+        assert_eq!(status.unwrap().code(), Some(1));
+    }
+    #[test]
+    fn fails_if_malformed() {
+        let status = Command::new("cargo")
+            .args(["run", "--", "check", "-d", "tests/fixtures/malformed"])
+            .status();
+        assert_eq!(status.unwrap().code(), Some(1));
+    }
 }
